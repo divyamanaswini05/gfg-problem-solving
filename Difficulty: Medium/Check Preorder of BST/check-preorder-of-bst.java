cd2a580b8caa7e0fe452e2b98f0fa86ad7b4956e
ALLADI DIVYA MANASWINI <divyamanaswini2006@gmail.com>
2026-07-23 10:15:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Preorder of BST/check-preorder-of-bst.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canRepresentBST(List<Integer> arr) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        int root=Integer.MIN_VALUE;
+        for(int val:arr){
+            if(val<root){
+                return false;
+            }
+            while(!stack.isEmpty()  && stack.peek()<val){
+                root = stack.pop();
+            }
+            stack.push(val);
+        }
+        return true;
+    }
+}
